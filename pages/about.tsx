147430d4dd4929dfa5ac64b47458a5b18555a963
timogoudzwaard <timo.goudzwaard@gmail.com>
2021-01-18 17:47:43 +0100

    initial nextjs setup with tailwind

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>About page</h1>
+        <p>This is the about page.</p>
+      </main>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.tsx", "./pages/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
