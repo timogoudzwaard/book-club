0141bb5bc29adce2ed817577373b5cac753fd5f6
Timo Goudzwaard <32880491+timogoudzwaard@users.noreply.github.com>
2021-01-31 16:13:28 +0100

    add storybook (#8)
    * add initial version of storybook
    
    * add button stories
    
    * add stories for card component, slight adjustments to card component

---
+++ components/atoms/button/button.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+import Button from './button';
+import '/.tailwind/tailwind.css';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+} as Meta;
+
+export const Default = () => <Button>Button text</Button>;
+
+export const Purple = () => <Button color="purple">Button text</Button>;
+
+export const Borderless = () => (
+  <Button variant="borderless">Button text</Button>
+);
--- components/atoms/card/card.tsx
+++ components/atoms/card/card.tsx
@@ -1,16 +1,16 @@
 type CardProps = {
-  padding?: boolean;
+  defaultPadding?: boolean;
   children: any;
   href?: string;
 };
 
-const Card = ({ padding, children, href }: CardProps) => {
+const Card = ({ defaultPadding, children, href }: CardProps) => {
   if (href) {
     return (
       <a
         className={`${
-          padding && 'p-6'
-        } pointer-cursor ring-1 ring-black ring-opacity-5 w-1/2 bg-white rounded-xl shadow-md items-start hover:bg-indigo-100 transition transform hover:scale-95 duration-500`}
+          defaultPadding && 'p-4'
+        } pointer-cursor ring-1 ring-black ring-opacity-5 bg-white rounded-xl shadow-md items-start hover:bg-indigo-100 transition transform hover:scale-95 duration-500`}
       >
         {children}
       </a>
@@ -19,8 +19,8 @@ const Card = ({ padding, children, href }: CardProps) => {
   return (
     <div
       className={`${
-        padding && 'p-6'
-      } ring-1 ring-black ring-opacity-5 w-1/2 bg-white rounded-xl shadow-md items-start`}
+        defaultPadding && 'p-4'
+      } ring-1 ring-black ring-opacity-5 bg-white rounded-xl shadow-md items-start`}
     >
       {children}
     </div>
